411cfa549fdc5103ca2b33b676ef9bbe9a4d01c2
WendyCoro <35311059+WendyCoro@users.noreply.github.com>
2018-02-22 17:04:19 -0500

    Update CuentaCreditoImpl.java

--- ejercicioBanco/src/ejerciciobanco/rnegocio/implementacion/CuentaCreditoImpl.java
+++ ejercicioBanco/src/ejerciciobanco/rnegocio/implementacion/CuentaCreditoImpl.java
@@ -4,11 +4,146 @@
  * and open the template in the editor.
  */
 package grupo5.rnegocio.impl;
+import ejerciciobanco.accesodatos.Conexion;
+import ejerciciobanco.accesodatos.Parametro;
+import ejerciciobanco.rnegocio.dao.*;
+import ejerciciobanco.rnegocio.entidades.*;
+import ejerciciobanco.rnegocio.implementacion.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author Wen
- */
-public class CuentaCreditoImpl {
+public class CuentaCorrienteImpl implements ICuentaCredito{
+     @Override
     
-}
+      public int insertar(CuentaCredito cuentacredito) throws Exception {
+        int numFilasAfectadas = 0;
+        String sql = "insert into CuentaCredito  values "
+                + "(?,?)";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, cuentacredito.getCodigocc()));
+        lstPar.add(new Parametro(2, cuentacredito.getValorsobregiro()));
+       
+       
+     
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public int modificar(CuentaCredito cuentacredito) throws Exception {
+        int numFilasAfectadas = 0;
+        String sql = "UPDATE CuentaCredito" + "   SET CodigoCC=?,Valorsobregiro=? "  + " where CodigoCC=?";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, cuentacredito.getCodigocc()));
+        lstPar.add(new Parametro(2, cuentacredito.getValorsobregiro()));
+        
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+public int eliminar(CuentaCredito cuentacredito) throws Exception {
+        int numFilasAfectadas = 0;
+         String sql = "DELETE FROM CuentaCredito  where CodigoCC=?";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, cuentacredito.getCodigocc()));       
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public CuentaCredito obtener(int codigocc) throws Exception {
+        CuentaCredito cuentacredito = null;
+        String sql = "SELECT *   FROM cuentaCredito where CodigoCC=?;";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, codigocc));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutaQuery(sql, lstPar);
+            while (rst.next()) {
+                cuentacredito = new CuentaCredito();
+                cuentacredito.setCodigocc(rst.getInt(1));
+                ICuenta cuentadao=new CuentaImpl();
+                cuentacredito.setValorsobregiro(rst.getDouble(2));
+                Cuenta cuenta=cuentadao.obtener(rst.getInt(3));
+                cuentacredito.setCuenta(cuenta);
+                
+            
+
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if(con!=null)
+            con.desconectar();
+        }
+        return cuentacredito;
+    }
+
+    @Override
+    
+    public List<CuentaCredito> obtener() throws Exception {
+        List<CuentaCredito> lista = new ArrayList<>();
+         String sql = "SELECT *   FROM CuentaCredito ";        
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutaQuery(sql, null);
+            CuentaCredito cuentacredito=null;
+            while (rst.next()) {
+                cuentacredito = new CuentaCredito();
+                cuentacredito.setCodigocc(rst.getInt(1));
+                cuentacredito.setValorsobregiro(rst.getInt(2));
+                ICuenta cuentadao=new CuentaImpl();
+                Cuenta cuenta=cuentadao.obtener(rst.getInt(3));
+                cuentacredito.setCuenta(cuenta);
+                
+                lista.add(cuentacredito);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if(con!=null)
+            con.desconectar();
+        }
+        return lista;
+    }
+
+   
+} 

9576eb7dd2f73e2fd8e8a4e1857467e368270d03
WendyCoro <35311059+WendyCoro@users.noreply.github.com>
2018-02-22 13:06:24 -0500

    Update Cuenta.java
    Correccion

--- ejercicioBanco/src/ejerciciobanco/rnegocio/entidades/Cuenta.java
+++ ejercicioBanco/src/ejerciciobanco/rnegocio/entidades/Cuenta.java
@@ -1,34 +1,88 @@
 
 package ejerciciobanco.rnegocio.entidades;
 
+
 public class Cuenta {
-    
-    private int numerocuenta;
-    private int saldo;
+    private Cliente cliente;
+    private Sucursal sucursal;
+    private Prestamo prestamo;
+    private String numerocuenta;
+    private  String tipocuenta;
+    private double saldo;
+    private double movimiento;
 
     public Cuenta() {
     }
 
-    public Cuenta(int numerocuenta, int saldo) {
+    public Cuenta(Cliente cliente, Sucursal sucursal, Prestamo prestamo, String numerocuenta, String tipocuenta, double saldo, double movimiento) {
+        this.cliente = cliente;
+        this.sucursal = sucursal;
+        this.prestamo = prestamo;
         this.numerocuenta = numerocuenta;
+        this.tipocuenta = tipocuenta;
         this.saldo = saldo;
+        this.movimiento = movimiento;
     }
 
-    public int getNumerocuenta() {
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Sucursal getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(Sucursal sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public Prestamo getPrestamo() {
+        return prestamo;
+    }
+
+    public void setPrestamo(Prestamo prestamo) {
+        this.prestamo = prestamo;
+    }
+
+    public String getNumerocuenta() {
         return numerocuenta;
     }
 
-    public void setNumerocuenta(int numerocuenta) {
+    public void setNumerocuenta(String numerocuenta) {
         this.numerocuenta = numerocuenta;
     }
 
-    public int getSaldo() {
+    public String getTipocuenta() {
+        return tipocuenta;
+    }
+
+    public void setTipocuenta(String tipocuenta) {
+        this.tipocuenta = tipocuenta;
+    }
+
+    public double getSaldo() {
         return saldo;
     }
 
-    public void setSaldo(int saldo) {
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
+
+    public double getMovimiento() {
+        return movimiento;
+    }
+
+    public void setMovimiento(double movimiento) {
+        this.movimiento = movimiento;
+    }
+    
+    
+    
+
     
     
 }

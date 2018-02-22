a76519c8ed9995bc3a44b6dfa66341fed88db9a0
WendyCoro <35311059+WendyCoro@users.noreply.github.com>
2018-02-22 13:09:39 -0500

    Add files via upload

---
+++ ejercicioBanco/src/ejerciciobanco/rnegocio/entidades/Cliente.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciobanco.rnegocio.entidades;
+
+
+public class Cliente {
+    private String Nombre;
+    private String Apellido;
+    private String telefono;
+    private String email; 
+    private int cedula;
+
+    public Cliente() {
+    }
+
+    public Cliente(String Nombre, String Apellido, String telefono, String email, int cedula) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+   
+}

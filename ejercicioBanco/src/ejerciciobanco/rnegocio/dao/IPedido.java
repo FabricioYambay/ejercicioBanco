b1b7a9261851cfa6a698a6a58447b1c9692cc3e1
eddyf <eddyf@172.30.61.19>
2018-02-22 11:27:11 -0500

    Creacion de Repositorio

---
+++ ejercicioBanco/src/ejerciciobanco/rnegocio/dao/IPedido.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciobanco.rnegocio.dao;
+
+/**
+ *
+ * @author eddyf
+ */
+public interface IPedido {
+    
+}
